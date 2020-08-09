99af25894098020dab1da7f5f10f53c3e7e0efbb
luis2 <luis212urbina@gmail.com>
2020-08-09 13:36:49 -0600

    Texture class and implementation almost done

--- gl.py
+++ gl.py
@@ -31,17 +31,15 @@ class Render(object):
         self.clear_color = BLACK
         self.glCreateWindow(width, height)
 
+        self.light = V3(0,0,1)
+        self.active_texture = None
+
     def glCreateWindow(self, width, height):
         self.width = width
         self.height = height
         self.glClear()
         self.glViewport(0,0, width, height)
 
-    def glClear(self):
-        self.pixels = [ [ self.clear_color for x in range(self.width)] for y in range(self.height) ]
-    
-        # Zbuffer
-        self.zbuffer = [ [ -float('inf') for x in range(self.width)] for y in range(self.height) ]
     def glViewport(self, x, y, width, height):
         self.viewport_initial_x = x
         self.viewport_initial_y = y
@@ -49,6 +47,12 @@ class Render(object):
         self.viewport_height = height
         self.viewport_final_x = x + width
         self.viewport_final_y = x + height
+
+    def glClear(self):
+        self.pixels = [ [ self.clear_color for x in range(self.width)] for y in range(self.height) ]
+    
+        # Zbuffer
+        self.zbuffer = [ [ -float('inf') for x in range(self.width)] for y in range(self.height) ]
         
     def glVertextInViewport(self, x,y):
         return (x >= self.viewport_initial_x and
@@ -69,6 +73,8 @@ class Render(object):
             pass
     
     def glPoint(self, x, y, color = None):
+        if x >= self.width or x < 0 or y >= self.height or y < 0:
+            return
         try:
             self.pixels[y][x] = color or self.curr_color
         except:
@@ -311,33 +317,57 @@ class Render(object):
                     v0 = V2(round(v0[0] * scale.x  + translate.x),round(v0[1] * scale.y  + translate.y))
                     v1 = V2(round(v1[0] * scale.x  + translate.x),round(v1[1] * scale.y  + translate.y))
                     self.glLine_coord(v0, v1)
+
             else:
                 v0 = model.vertices[ face[0][0] - 1 ]
                 v1 = model.vertices[ face[1][0] - 1 ]
                 v2 = model.vertices[ face[2][0] - 1 ]
+                if vertCount > 3:
+                    v3 = model.vertices[ face[3][0] - 1 ]
 
                 v0 = self.transform(v0,translate, scale)
                 v1 = self.transform(v1,translate, scale)
                 v2 = self.transform(v2,translate, scale)
-                    
-                
-                normal = cross(substract(v1,v0), substract(v2,v0))
+                if vertCount > 3:
+                    v3 = self.transform(v3,translate, scale)
 
- 
+                if self.active_texture:
+                    vt0 = model.texcoords[face[0][1] - 1]
+                    vt1 = model.texcoords[face[1][1] - 1]
+                    vt2 = model.texcoords[face[2][1] - 1]
+                    vt0 = V2(vt0[0], vt0[1])
+                    vt1 = V2(vt1[0], vt1[1])
+                    vt2 = V2(vt2[0], vt2[1])
+                    if vertCount > 3:
+                        vt3 = model.texcoords[face[3][1] - 1]
+                        vt3 = V2(vt3[0], vt3[1])
+                else:
+                    vt0 = V2(0,0) 
+                    vt1 = V2(0,0) 
+                    vt2 = V2(0,0) 
+                    vt3 = V2(0,0)
+
+                # vn0 = model.normals[face[0][2] - 1]
+                # vn1 = model.normals[face[1][2] - 1]
+                # vn2 = model.normals[face[2][2] - 1]
+                # if vertCount > 3:
+                #     vn3 = model.normals[face[3][2] - 1]
+
+                normal = cross(substract(v1,v0), substract(v2,v0))
                 intensity = dot(norm(normal), norm(light))
 
                 if intensity >=0:
-                    self.triangle_bc(v0,v1,v2, color(intensity, intensity, intensity))
+                    self.triangle_bc(v0,v1,v2, texture=self.active_texture, texcoords=(vt0,vt1, vt2), intensity=intensity)
 
-                # Manage square rendering
-                if vertCount > 3:
-                    v3 = model.vertices[ face[3][0] - 1 ]
-                    v3 = self.transform(v3,translate, scale)
-                    if intensity >=0:
-                        self.triangle_bc(v0,v2,v3, color(intensity, intensity, intensity))
+                    # Manage square rendering
+                    if vertCount > 3:
+                        v3 = model.vertices[ face[3][0] - 1 ]
+                        v3 = self.transform(v3,translate, scale)
+                        if intensity >=0:
+                            self.triangle_bc(v0,v2,v3, color(intensity, intensity, intensity))
 
     #Barycentric Coordinates
-    def triangle_bc(self, A, B, C, color = None):
+    def triangle_bc(self, A, B, C, texture, _color= WHITE,texcoords = (), intensity = 1):
         #bounding box
         minX = min(A.x, B.x, C.x)
         minY = min(A.y, B.y, C.y)
@@ -346,15 +376,48 @@ class Render(object):
 
         for x in range(minX, maxX + 1):
             for y in range(minY, maxY + 1):
+                if x >= self.width or x < 0 or y >= self.height or y < 0:
+                    continue
+
                 u, v, w = baryCoords(A, B, C, V2(x, y))
 
                 if u >= 0 and v >= 0 and w >= 0:
 
                     z = A.z * u + B.z * v + C.z * w
-
                     if z > self.zbuffer[y][x]:
-                        self.glPoint(x, y, color)
+
+                        b, g, r = _color
+                        b/= 255
+                        g/= 255
+                        r/= 255
+
+                        b*= intensity
+                        g*= intensity
+                        r*= intensity
+
+                        if texture:
+                            ta, tb, tc = texcoords
+
+                            tx = ta.x * u + tb.x * v + tc.x*w
+                            ty = ta.y * u + tb.y * v + tc.y*w
+
+                            texColor = texture.getColor(tx,ty)
+                            b*=texColor[0] /255
+                            g*=texColor[1] /255
+                            r*=texColor[2] /255
+
+                        # r, g, b = self.active_shader(
+                        #     self,
+                        #     verts=(A,B,C),
+                        #     baryCoords=(u,v,w),
+                        #     texCoords=texcoords,
+                        #     normals=normals,
+                        #     color = _color or self.curr_color)
+
+                        self.glPoint(x, y, color(r,g,b))
                         self.zbuffer[y][x] = z
+
+
 
 
 
--- obj.py
+++ obj.py
@@ -1,4 +1,7 @@
-# Opens and reads an .obj file saving it's information in four arrays
+import struct
+
+def color(r, g, b):
+    return bytes([int(b * 255), int(g * 255), int(r * 255)])
 
 class Obj(object):
     def __init__(self, filename):
@@ -14,18 +17,57 @@ class Obj(object):
 
     def read(self):
         for line in self.lines:
-            if line and (line[0] == 'v' or line[0] == 'f'):
-                prefix, value = line.split(' ', 1)
+            if line:
+                try:
+                    prefix, value = line.split(' ', 1)
+                except:
+                    continue
 
-                # Split the vertices
-                if prefix == 'v':
+                if prefix == 'v': # vertices
                     self.vertices.append(list(map(float,value.split(' '))))
-                # Split the normals
                 elif prefix == 'vn':
                     self.normals.append(list(map(float,value.split(' '))))
-                # Split the texcoords
                 elif prefix == 'vt':
                     self.texcoords.append(list(map(float,value.split(' '))))
-                # Split the faces
                 elif prefix == 'f':
                     self.faces.append([list(map(int,vert.split('/'))) for vert in value.split(' ')])
+
+
+class Texture(object):
+    def __init__(self, path):
+        self.path = path
+        self.read()
+        
+    def read(self):
+        image = open(self.path, 'rb')
+        image.seek(10)
+        headerSize = struct.unpack('=l', image.read(4))[0]
+
+        image.seek(14 + 4)
+        self.width = struct.unpack('=l', image.read(4))[0]
+        self.height = struct.unpack('=l', image.read(4))[0]
+        image.seek(headerSize)
+
+        self.pixels = []
+
+        for y in range(self.height):
+            self.pixels.append([])
+            for x in range(self.width):
+                b = ord(image.read(1)) / 255
+                g = ord(image.read(1)) / 255
+                r = ord(image.read(1)) / 255
+                self.pixels[y].append(color(r,g,b))
+
+        image.close()
+
+    def getColor(self, tx, ty):
+        if tx >= 0 and tx <= 1 and ty >= 0 and ty <= 1:
+            x = int(tx * self.width - 1)
+            y = int(ty * self.height - 1)
+
+            return self.pixels[y][x]
+        else:
+            return color(0,0,0)
+
+
+
